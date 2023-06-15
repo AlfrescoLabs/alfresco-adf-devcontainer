bae2b9f42d287bb409e8643490b696de66e3750c
vprovaggi <valerio.provaggi@hyland.com>
2023-06-15 17:00:47 +0200

    removed the need of sudo

--- .devcontainer/feat-add-adf-generator/install.sh
+++ .devcontainer/feat-add-adf-generator/install.sh
@@ -4,4 +4,3 @@ set -e
 ADF_GENERATOR_VERSION=${ADFGENERATORVERSION:-undefined}
 echo "INSTALLING Yeoman Generator version $ADF_GENERATOR_VERSION"
 su $_REMOTE_USER -c "npm install -g yo && npm install -g generator-alfresco-adf-app@$ADF_GENERATOR_VERSION"
-echo " Done "
--- .devcontainer/feat-nx-workspace-on-volume/install.sh
+++ .devcontainer/feat-nx-workspace-on-volume/install.sh
@@ -1,3 +1,8 @@
 #!/bin/sh
--e
-echo "Mounting nxcache and .angular on volumes"
+set -e
+
+echo "Mounting nxcache and .angular on a volume, this will improve performance. Remember to not delete the folder manually and use rm -rf node_module/* instead"
+WORKSPACE_FOLDER=${WORKSPACEFOLDER:-$_REMOTE_USER_HOME}
+
+mkdir -p $WORKSPACE_FOLDER/nxcache $WORKSPACE_FOLDER/.angular 
+chown $_REMOTE_USER:$_REMOTE_USER $WORKSPACE_FOLDER/nxcache $WORKSPACE_FOLDER/.angular 
